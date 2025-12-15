cbf62a707ac4f7a305a3ab42a3c8f5f0fdff70af
Carl Kwan <>
2025-12-15 06:00:31 -0800

    Handling structs, literals, and reformatting output for LLZK to ZKLean IR conversion

--- include/llzk/Conversions/Passes.h
+++ include/llzk/Conversions/Passes.h
@@ -1,10 +1,5 @@
 //===-- Passes.h -------------------------------------------------*- C++ -*-===//
 //
-// Part of the LLZK Project, under the Apache License v2.0.
-// See LICENSE.txt for license information.
-// Copyright 2025 Veridise Inc.
-// SPDX-License-Identifier: Apache-2.0
-//
 //===----------------------------------------------------------------------===//
 
 #pragma once
