345cbd1980700b09209512ce7742078e5aa03081
Tim Hoffman <timothy.hoffman@veridise.com>
2026-03-03 23:08:56 -0600

    fix version number parsing

--- cmake/GitVersion.cmake
+++ cmake/GitVersion.cmake
@@ -15,6 +15,9 @@ function(get_git_version GIT_VERSION_VAR)
     # `N` is the number of commits since the last tag, and `HASH` is the abbreviated commit hash. We need to
     # convert this to `X.Y.Z.N` for the LLZK version number.
     string(REGEX REPLACE "^v?([0-9]+\\.[0-9]+\\.[0-9]+)-([0-9]+)-g[0-9a-f]+" "\\1.\\2" VERSION_NUMBER "${GIT_VERSION}")
+    # If the current commit is exactly at a tag, the output of `git describe --tags` is just `vX.Y.Z` which
+    # is not matched by the previous case so handle that as well.
+    string(REGEX REPLACE "^v" "" VERSION_NUMBER "${VERSION_NUMBER}")
     set(${GIT_VERSION_VAR} "${VERSION_NUMBER}" PARENT_SCOPE)
   endif()
 endfunction()
