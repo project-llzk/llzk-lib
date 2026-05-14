3ef89004c053f619f229291ed08c2e87e20df651
tim-hoffman <tim-hoffman@users.noreply.github.com>
2026-05-14 22:46:53 +0000

    docs: update main documentation

--- main/structllzk_1_1LLZKDialectBytecodeInterface.js
+++ main/structllzk_1_1LLZKDialectBytecodeInterface.js
@@ -2,7 +2,7 @@ var structllzk_1_1LLZKDialectBytecodeInterface =
 [
     [ "LLZKDialectBytecodeInterface", "structllzk_1_1LLZKDialectBytecodeInterface.html#ad3b46fd55fe8865fb43dd6923a86c0b5", null ],
     [ "readVersion", "structllzk_1_1LLZKDialectBytecodeInterface.html#a1bec72a5c477ecc2422fec411ae99e43", null ],
-    [ "upgradeFromVersion", "structllzk_1_1LLZKDialectBytecodeInterface.html#a10ec2818e60eab70eef6563d1c936b8d", null ],
+    [ "upgradeFromVersion", "structllzk_1_1LLZKDialectBytecodeInterface.html#a8323c6336f8bdd5b1951688f40b3c069", null ],
     [ "upgradeFromVersion", "structllzk_1_1LLZKDialectBytecodeInterface.html#a18418dd87b2b0aa8247e414abceecae3", null ],
     [ "writeVersion", "structllzk_1_1LLZKDialectBytecodeInterface.html#aafabdbbceef8d9f14ae8652b35c1aee5", null ]
 ];
