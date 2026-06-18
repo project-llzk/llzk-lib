003f87fdd3f8e11cfcce0ae85fa3a5145ed1d39a
Ian Glen Neal <ian.gl.neal@gmail.com>
2026-06-18 16:30:47 -0400

    Rename more files

--- backends/smt/lib/Transforms/SMTDialectToSMTLIB.cpp
+++ backends/smt/lib/Transforms/SMTDialectToSMTLIB.cpp
@@ -1,4 +1,4 @@
-//===-- PrettyPrintSMTLIB.cpp -----------------------------------*- C++ -*-===//
+//===-- SMTDialectToSMTLIB.cpp ----------------------------------*- C++ -*-===//
 //
 // Part of the LLZK Project, under the Apache License v2.0.
 // See LICENSE.txt for license information.
