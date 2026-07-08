456af8a99f46ca12b3232a8326338dabd32cc6fd
tim-hoffman <tim-hoffman@users.noreply.github.com>
2026-07-08 20:55:52 +0000

    docs: update main documentation

--- main/namespaces_dup.js
+++ main/namespaces_dup.js
@@ -7,12 +7,12 @@ var namespaces_dup =
     [ "llzk", "namespacellzk.html", "namespacellzk" ],
     [ "mlir", "namespacemlir.html", "namespacemlir" ],
     [ "mlir_hotfix", "namespacemlir__hotfix.html", null ],
-    [ "Step1A_InstantiateStructs", "namespaceStep1A__InstantiateStructs.html", null ],
-    [ "Step1B_InstantiateFunctions", "namespaceStep1B__InstantiateFunctions.html", null ],
-    [ "Step2_Unroll", "namespaceStep2__Unroll.html", null ],
-    [ "Step3_InstantiateAffineMaps", "namespaceStep3__InstantiateAffineMaps.html", null ],
-    [ "Step4_PropagateTypes", "namespaceStep4__PropagateTypes.html", null ],
-    [ "Step5_Cleanup", "namespaceStep5__Cleanup.html", null ],
+    [ "Step1_InstantiateStructs", "namespaceStep1__InstantiateStructs.html", null ],
+    [ "Step2_InstantiateFunctions", "namespaceStep2__InstantiateFunctions.html", null ],
+    [ "Step3_Unroll", "namespaceStep3__Unroll.html", null ],
+    [ "Step4_InstantiateAffineMaps", "namespaceStep4__InstantiateAffineMaps.html", null ],
+    [ "Step5_PropagateTypes", "namespaceStep5__PropagateTypes.html", null ],
+    [ "Step6_Cleanup", "namespaceStep6__Cleanup.html", null ],
     [ "test_templates", "namespacetest__templates.html", null ],
     [ "WildcardFunctionSpecialization", "namespaceWildcardFunctionSpecialization.html", null ]
 ];
--- main/search/namespaces_3.js
+++ main/search/namespaces_3.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['step1a_5finstantiatestructs_0',['Step1A_InstantiateStructs',['../namespaceStep1A__InstantiateStructs.html',1,'']]],
-  ['step1b_5finstantiatefunctions_1',['Step1B_InstantiateFunctions',['../namespaceStep1B__InstantiateFunctions.html',1,'']]],
-  ['step2_5funroll_2',['Step2_Unroll',['../namespaceStep2__Unroll.html',1,'']]],
-  ['step3_5finstantiateaffinemaps_3',['Step3_InstantiateAffineMaps',['../namespaceStep3__InstantiateAffineMaps.html',1,'']]],
-  ['step4_5fpropagatetypes_4',['Step4_PropagateTypes',['../namespaceStep4__PropagateTypes.html',1,'']]],
-  ['step5_5fcleanup_5',['Step5_Cleanup',['../namespaceStep5__Cleanup.html',1,'']]]
+  ['step1_5finstantiatestructs_0',['Step1_InstantiateStructs',['../namespaceStep1__InstantiateStructs.html',1,'']]],
+  ['step2_5finstantiatefunctions_1',['Step2_InstantiateFunctions',['../namespaceStep2__InstantiateFunctions.html',1,'']]],
+  ['step3_5funroll_2',['Step3_Unroll',['../namespaceStep3__Unroll.html',1,'']]],
+  ['step4_5finstantiateaffinemaps_3',['Step4_InstantiateAffineMaps',['../namespaceStep4__InstantiateAffineMaps.html',1,'']]],
+  ['step5_5fpropagatetypes_4',['Step5_PropagateTypes',['../namespaceStep5__PropagateTypes.html',1,'']]],
+  ['step6_5fcleanup_5',['Step6_Cleanup',['../namespaceStep6__Cleanup.html',1,'']]]
 ];
