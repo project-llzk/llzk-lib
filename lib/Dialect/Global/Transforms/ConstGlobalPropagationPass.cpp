63e5bf2c6a1f98e5498d16278b74f03a9728a370
Tim Hoffman <timothy.hoffman@veridise.com>
2026-08-21 17:01:43 -0500

    add pass implementation

--- lib/Dialect/Global/Transforms/ConstGlobalPropagationPass.cpp
+++ lib/Dialect/Global/Transforms/ConstGlobalPropagationPass.cpp
@@ -12,14 +12,20 @@
 ///
 //===----------------------------------------------------------------------===//
 
+#include "llzk/Analysis/SymbolUseGraph.h"
 #include "llzk/Dialect/Global/IR/Ops.h"
 #include "llzk/Dialect/Global/Transforms/TransformationPasses.h"
+#include "llzk/Util/Debug.h"
+#include "llzk/Util/SymbolTableLLZK.h"
+#include "llzk/Util/Walk.h"
 
 // Include the generated base pass class definitions.
 namespace llzk::global {
 #define GEN_PASS_DEF_CONSTGLOBALPROPAGATIONPASS
 #include "llzk/Dialect/Global/Transforms/TransformationPasses.h.inc"
 } // namespace llzk::global
+
+#define DEBUG_TYPE "llzk-const-global-propagation"
 
 using namespace mlir;
 using namespace llzk;
@@ -34,7 +40,38 @@ public:
   using Base::Base;
 
   void runOnOperation() override {
-    // TODO: find uses of const global symbols and replace them with their values.
+    ModuleOp root = getOperation();
+    auto constGlobals = walkCollect<GlobalDefOp>(root, [](auto g) { return g.isConstant(); });
+    if (constGlobals.empty()) {
+      return;
+    }
+
+    SymbolUseGraph &useGraph = getAnalysis<SymbolUseGraph>();
+    for (GlobalDefOp globalDef : constGlobals) {
+      if (const SymbolUseGraphNode *node = useGraph.lookupNode(globalDef)) {
+        SymbolRefAttr symbolAttr = node->getSymbolPath();
+        Attribute constValue = globalDef.getInitialValueAttr();
+        for (Operation *userOp : node->getUserOps()) {
+          LLVM_DEBUG(
+              llvm::outs() << "Replacing '" << symbolAttr << "' with '" << constValue << "' in "
+                           << *userOp << '\n'
+          );
+
+          AttrTypeReplacer replacer;
+          replacer.addReplacement([symbolAttr, constValue](SymbolRefAttr a) {
+            return (a == symbolAttr) ? std::make_optional(constValue) : std::nullopt;
+          });
+          replacer.replaceElementsIn(
+              userOp,
+              /*replaceAttrs=*/true,
+              /*replaceLocs=*/false,
+              /*replaceTypes=*/true
+          );
+        }
+      }
+      // Delete the const global since there are no (remaining) uses.
+      globalDef.erase();
+    }
   }
 };
 
