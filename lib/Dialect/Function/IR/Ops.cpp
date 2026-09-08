58a0c9ad638f8e266244ba478dd0d2566db025ce
Tim Hoffman <timothy.hoffman@veridise.com>
2026-09-08 11:13:22 -0500

    fix: Return the result for struct product calls

--- lib/Dialect/Function/IR/Ops.cpp
+++ lib/Dialect/Function/IR/Ops.cpp
@@ -24,6 +24,7 @@
 #include "llzk/Util/AffineHelper.h"
 #include "llzk/Util/BuilderHelper.h"
 #include "llzk/Util/Compare.h"
+#include "llzk/Util/ErrorHelper.h"
 #include "llzk/Util/SymbolHelper.h"
 #include "llzk/Util/SymbolLookup.h"
 #include "llzk/Util/SymbolTableLLZK.h"
@@ -475,23 +476,35 @@ SymbolRefAttr FuncDefOp::getFullyQualifiedName(bool requireParent) {
   return llzk::getFullyQualifiedName(*this, requireParent);
 }
 
-Value FuncDefOp::getSelfValueFromCompute() {
-  assert(nameIsCompute()); // skip inStruct check to allow dangling functions
+namespace {
+
+static Value getSelfValueFromWitnessGen(FuncDefOp funcDefOp, Twine name) {
   // Get the single block of the function body
-  Region &body = getBody();
-  assert(!body.empty() && "compute() function body is empty");
-  Block &block = body.back();
+  Region &body = funcDefOp.getBody();
+  ensure(!body.empty(), name + "() function body must not be empty");
 
   // The terminator should be the return op
-  Operation *terminator = block.getTerminator();
-  assert(terminator && "compute() function has no terminator");
+  Operation *terminator = body.back().getTerminator();
+  ensure(terminator, name + "() function must have a terminator op");
   auto retOp = llvm::dyn_cast<ReturnOp>(terminator);
   if (!retOp) {
     llvm::errs() << "Expected '" << ReturnOp::getOperationName() << "' but found '"
                  << terminator->getName() << "'\n";
-    llvm_unreachable("compute() function must end with ReturnOp");
+    ensure(false, name + "() function must end with ReturnOp");
   }
   return retOp.getOperands().front();
+}
+
+} // namespace
+
+Value FuncDefOp::getSelfValueFromCompute() {
+  assert(nameIsCompute()); // skip inStruct check to allow dangling functions
+  return getSelfValueFromWitnessGen(*this, "compute");
+}
+
+Value FuncDefOp::getSelfValueFromProduct() {
+  assert(nameIsProduct()); // skip inStruct check to allow dangling functions
+  return getSelfValueFromWitnessGen(*this, "product");
 }
 
 Value FuncDefOp::getSelfValueFromConstrain() {
@@ -1074,16 +1087,23 @@ Value CallOp::getSelfValueFromCompute() {
   return getResults().front();
 }
 
+Value CallOp::getSelfValueFromProduct() {
+  assert(calleeIsStructProduct());
+  return getResults().front();
+}
+
 Value CallOp::getSelfValueFromConstrain() {
   assert(calleeIsStructConstrain());
   return getArgOperands().front();
 }
 
 Value CallOp::getSelfValue() {
-  if (calleeIsStructCompute()) {
-    return getSelfValueFromCompute();
-  } else if (calleeIsStructConstrain()) {
+  if (calleeIsStructConstrain()) {
     return getSelfValueFromConstrain();
+  } else if (calleeIsStructCompute()) {
+    return getSelfValueFromCompute();
+  } else if (calleeIsStructProduct()) {
+    return getSelfValueFromProduct();
   } else {
     return nullptr;
   }
