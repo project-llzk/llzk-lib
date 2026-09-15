e9f7a82b5977f3c0e90605511befdd6d08f41db1
shankarapailoor <shankarapailoor@gmail.com>
2026-09-15 16:15:45 -0700

    Reject aliased POD while initializers

--- lib/Dialect/POD/Transforms/PodToScalarPass.cpp
+++ lib/Dialect/POD/Transforms/PodToScalarPass.cpp
@@ -111,6 +111,7 @@
 #include <mlir/Transforms/Passes.h>
 
 #include <llvm/ADT/DenseMapInfo.h>
+#include <llvm/ADT/DenseSet.h>
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/TypeSwitch.h>
 #include <llvm/Support/Debug.h>
@@ -6365,6 +6366,7 @@ public:
 
     Block &beforeBody = *whileOp.getBeforeBody();
     Block &afterBody = *whileOp.getAfterBody();
+    llvm::DenseSet<Value> uniquePodInits;
     for (const WhileCarriedPod &pod : carriedPods) {
       if (hasUnsupportedWhileCarriedPodUse(whileOp.getBeforeArguments()[pod.originalIndex], pod) ||
           hasUnsupportedWhileCarriedPodUse(whileOp.getAfterArguments()[pod.originalIndex], pod)) {
@@ -6372,6 +6374,10 @@ public:
       }
 
       Value init = whileOp.getInits()[pod.originalIndex];
+      // Splitting two aliases independently would lose writes observed through the other alias.
+      if (!uniquePodInits.insert(init).second) {
+        return failure();
+      }
       for (Operation *user : init.getUsers()) {
         if (user != whileOp.getOperation() &&
             !(user->getBlock() == whileOp->getBlock() && user->isBeforeInBlock(whileOp))) {
